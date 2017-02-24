17fa1b9283bcdd3366b917a7006a60bdd0f22085
ehugh <ehugh@DESKTOP-6D85Q1O>
2017-02-23 20:13:40 -0500

    created project folders and classes

---
+++ Pacman/Game/Classes/Map/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Pacman.Characters.Interfaces;
+
+namespace Pacman.Game.Classes.Map
+{
+    public abstract class Tile 
+    {
+        public Tile(int x, int y) { }
+        public Vector2 Position;
+        public ICollidable Member;
+        public Boolean CanEnter();
+        public void Collide();
+    }
+}
