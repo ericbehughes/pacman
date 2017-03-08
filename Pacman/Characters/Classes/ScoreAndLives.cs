1d5113f5d1ae0f3e9ea1abc2188c83024bbdf5a8
1232983 <1232983@2F16-28>
2017-03-08 17:34:05 -0500

    hjkl

---
+++ Pacman/Characters/Classes/ScoreAndLives.cs
@@ -0,0 +1,51 @@
+﻿using Pacman.Characters.Interfaces;
+using Pacman.Game.Classes.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman.Characters.Classes
+{
+    // game over delegate over these 2
+    
+    public delegate void GameOverHandler();
+    class ScoreAndLives
+    {
+        private GameState gamestate;
+        public event GameOverHandler GameOver;
+        private int lives;
+        private int score;
+        public ScoreAndLives(GameState gs)
+        {
+            this.gamestate = gs;
+        }
+
+        public int Lives
+        {
+            get { return this.lives; }
+            set { this.lives = value; }
+        }
+        public int Score
+        {
+            get { return this.score; }
+            set { this.score = value; }
+        }
+
+        protected virtual void OnGameOver()
+        {
+            OnGameOver();
+        }
+
+        public void deadPacman()
+        {
+            if (lives <= 0)
+            {
+                GameOver();
+            }
+
+            // 
+        }
+    }
+}
